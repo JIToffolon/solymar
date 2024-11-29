2dc975d3858fdfe1a5f0d970b09e16eeab05ee07
Juan Ignacio Toffolon <juanignaciotoffolon@gmail.com>
2024-11-29 14:16:16 -0300

    dynamic notfound

--- app/_not-found/page.js
+++ app/_not-found/page.js
@@ -1,8 +1,5 @@
-export default function NotFound() {
-    return (
-      <div className="flex flex-col items-center justify-center min-h-screen">
-        <h2 className="text-2xl font-bold mb-4">Página no encontrada</h2>
-        <p>Lo sentimos, la página que buscas no existe.</p>
-      </div>
-    );
-  }
+export const dynamic = 'force-dynamic';
+
+export default function NotFoundPage() {
+  return <div>Página no encontrada</div>;
+}
